97a35e6b71185f2fa477a612223f7e7086927814
Jacob Faulk <jacob5567@gmail.com>
2019-09-24 10:48:19 -0700

    Added tournament files

---
+++ PlayerO.py
@@ -0,0 +1,8 @@
+def load_player(player):
+    # insert file reading code here -- whatever is returned by this function will be saved as lookupO and passed into
+    # next_move for use when choosing a move.
+    return None
+
+def next_move(board, player, lookup_table):
+    # insert code to select move -- until this code is updated, player will play randomly
+    return None

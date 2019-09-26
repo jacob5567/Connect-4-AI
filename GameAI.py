3cb4834deb7d8c079bbc6a5572880352fdf55b7c
Jacob Faulk <jacob5567@gmail.com>
2019-09-26 11:24:37 -0700

    added early move structure

--- GameAI.py
+++ GameAI.py
@@ -1,17 +1,30 @@
 # Jacob Faulk
 
 from GameInstance import GameInstance
+from Move import Move
+from Connect4 import C4Game
+import random
 
 
 def main():
-    firstMove = GameInstance()
-    print(firstMove)
-    secondMove = GameInstance(2, 'X', firstMove)
-    print(secondMove)
-    thirdMove = GameInstance(4, 'O', secondMove)
-    print(thirdMove)
-    print(thirdMove.parent)
-    print(firstMove)
+    firstMove = Move()
+    for i in range(7):
+        firstMove.add_child(Move(i, 'X', firstMove))
+
+    for i in range(7):
+        for j in range(7):
+            firstMove.children[i].add_child(
+                Move(j, 'O', firstMove.children[i]))
+
+    game = C4Game()
+    currentMove = firstMove
+    while len(currentMove.children) > 0:
+        print(currentMove)
+        # print(game)
+        nextMove = random.choice(currentMove.children)
+        game.make_move(nextMove.col)
+        currentMove = nextMove
+    print(game)
 
 
 if __name__ == "__main__":
---
+++ Move.py
@@ -0,0 +1,14 @@
+class Move:
+
+    children = []
+
+    def __init__(self, col=None, player=None, parent=None):
+        self.col = col
+        self.player = player
+        self.parent = parent
+
+    def add_child(self, child):
+        self.children.append(child)
+
+    def __str__(self):
+        return str(self.col) + str(self.player)
